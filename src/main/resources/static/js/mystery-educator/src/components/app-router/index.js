57448fea11b2c7781fced7f9f09e56a3a5ff2503
PrinceM95 <moutimap@gmail.com>
2023-01-24 11:17:51 -0500

    fixed 'switch' error in app-router, index.js file. app now online

--- src/main/resources/static/js/mystery-educator/src/components/app-router/index.js
+++ src/main/resources/static/js/mystery-educator/src/components/app-router/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
+
 import HomePage from '../../pages/home'
+import React from 'react';
 
 const PageSwitch = () => (
-    <Switch>
+    <Routes>
         <Route exact path={'/'} component={HomePage} />
-    </Switch>
+    </Routes>
 );
 
 export default PageSwitch;
