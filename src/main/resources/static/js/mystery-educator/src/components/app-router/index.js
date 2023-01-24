3357634021a11dc869642c9001b4bddbb073f9a2
campbene <campbell20nate@gmail.com>
2023-01-24 16:12:51 -0500

    fixes to router file to meet updated react-router-dom requirements

--- src/main/resources/static/js/mystery-educator/src/components/app-router/index.js
+++ src/main/resources/static/js/mystery-educator/src/components/app-router/index.js
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 
 import HomePage from '../../pages/home'
 import React from 'react';
+import ContactPage from '../../pages/contact';
 
 const PageSwitch = () => (
     <Routes>
-        <Route exact path={'/'} component={HomePage} />
+        <Route exact path='/' element={<HomePage />} />
+        <Route exact path={'/contact'} element={<ContactPage />} />
     </Routes>
 );
 
---
+++ src/main/resources/static/js/mystery-educator/src/pages/contact/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h2>Meet the Devs</h2>
+        </div>
+    );
+}
+
+export default ContactPage;
