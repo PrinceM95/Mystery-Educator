700c8c2de5411cdb8f3b84aebe2304a88626c37f
Donna Magbag <111684703+DMagbag@users.noreply.github.com>
2023-01-26 15:37:34 -0500

    added table-of-contents page

--- src/main/resources/static/js/mystery-educator/src/components/app-router/index.js
+++ src/main/resources/static/js/mystery-educator/src/components/app-router/index.js
@@ -5,10 +5,12 @@ import HomePage from '../../pages/home'
 import InstrumentPage from '../../pages/instrument';
 import InstrumentsPage from '../../pages/instruments';
 import React from 'react';
+import TableOfContentsPage from '../../pages/table-of-contents';
 
 const PageSwitch = () => (
     <Routes>
         <Route exact path='/' element={<HomePage />} />
+        <Route exact path={'/table-of-contents'} element={<TableOfContentsPage />} />
         <Route exact path={'/contact'} element={<ContactPage />} />
         <Route exact path={'/instruments'} element={<InstrumentsPage />} />
         <Route exact path={'/instruments/:id'} element={<InstrumentPage />} />
