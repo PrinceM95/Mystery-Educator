36257771a01cf4dd9a317a74d7964ad0110a3b65
PrinceM95 <moutimap@gmail.com>
2023-01-30 16:34:45 -0500

    added slideshow to home page. began working on hover effect on instruments page

--- src/main/resources/static/js/mystery-educator/src/pages/home/index.js
+++ src/main/resources/static/js/mystery-educator/src/pages/home/index.js
@@ -1,48 +1,17 @@
 import React from 'react';
 import style from './style.module.scss';
 import star from '../../assets/mag-glass.png';
-import logo from '../../assets/logo_darkred.gif';
+import logo from '../../assets/logos2.gif';
 
 const HomePage = () => {
-// const colors = ["#0088FE", "#00C49F", "#FFBB28"];
-// const delay = 2500;
-
-// function Slideshow() {
-//     const [index, setIndex] = React.useState(0);
-  
-//     React.useEffect(() => {
-//       setTimeout(
-//         () =>
-//           setIndex((prevIndex) =>
-//             prevIndex === colors.length - 1 ? 0 : prevIndex + 1
-//           ),
-//         delay
-//       );
-
-//       return () => {};
-//     }, [index]);
 
     return (
         <div className={style.home}>
-         {/* <div className="slideshow">
-      <div
-        className="slideshowSlider"
-        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-      ></div>
-      </div>
-        {colors.map((backgroundColor, index) => (
-          <div
-            className="slide"
-            key={index}
-            style={{ backgroundColor }}
-          ></div>
-        ))} */}
-            {/* <div id={style.page_title}> Jac's Mystery Educator! </div> */}
             <div id={style.page_quote}> "Vincit Qui Se Vincit" </div>
             <img src={star} className={style.star_img} alt="shooting star"></img>
             <img src={logo} className={style.logo} alt="logo"></img>
-        </div>  
+        </div>
     ); 
-}
+  }
 
 export default HomePage;
--- src/main/resources/static/js/mystery-educator/src/pages/instruments/index.js
+++ src/main/resources/static/js/mystery-educator/src/pages/instruments/index.js
@@ -34,22 +34,51 @@ const InstrumentsPage = () => {
 
     return (
         <div>
-            <h2>Our Instruments Page</h2>
-        <div className={style.instrument_icons}> 
-            <span><img src={guitar} className={style.icon_img} alt="guitar"></img></span>
-            <span><img src={piano} className={style.icon_img2} alt="piano"></img></span>
-            <span><img src={violin} className={style.icon_img3} alt="violin"></img></span>
-            <span><img src={drums} className={style.icon_img4} alt="drums"></img></span>
-            <span><img src={flute} className={style.icon_img5} alt="flute"></img></span>
+        <div className={style.instrument_icons}>
+        <h2>MUSIC EXPLORATION</h2>
+        <div class={style.container}>
+            <span><img src={guitar} class={style.image} alt="guitar"></img></span>
+            <div class={style.overlay}>
+            <div class={style.projeto1}>GUITAR</div>
         </div>
+        </div>
+
+        <div class={style.container}>
+            <span><img src={piano} className={style.image} alt="piano" class="image"></img></span>
+            <div class={style.overlay}>
+            <div class={style.projeto1}>PIANO</div>
+        </div>
+        </div>
+
+        <div class={style.container}>
+            <span><img src={violin} className={style.image} alt="violin" class="image"></img></span>
+            <div class={style.overlay}>
+            <div class={style.projeto1}>VIOLIN</div>
+        </div>
+        </div>
+
+        <div class={style.container}>
+            <span><img src={drums} className={style.image} alt="drums" class="image"></img></span>
+            <div class={style.overlay}>
+            <div class={style.projeto1}>DRUMS</div>
+        </div>
+        </div>
+
+        <div class={style.container}>
+            <span><img src={flute} className={style.image} alt="flute" class="image"></img></span>
+            <div class={style.overlay}>
+            <div class={style.projeto1}>FLUTE</div>
+        </div>
+        </div>
+    </div>
             {/* {loading ? <h3>Loading...</h3> : instruments.map(instrument => (
                 <a key={instrument.id} href={`instruments/${instrument.id}`}>
                     <p>{instrument.instrumentName}</p>
-                </a> */}
-            {/* ))} */}
-        </div>
-        
+                </a>
+            ))} */}
+        </div>  
     );
 }
+
 
 export default InstrumentsPage;
