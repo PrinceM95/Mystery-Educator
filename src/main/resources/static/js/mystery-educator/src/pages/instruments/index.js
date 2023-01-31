55b428f5e34a39e45e6703b43da55b564f6ccfa8
PrinceM95 <moutimap@gmail.com>
2023-01-31 15:28:25 -0500

    added quote in slideshow, a pic in bored, enhanced instruments page, and added a pic in nasa

--- src/main/resources/static/js/mystery-educator/src/pages/instruments/index.js
+++ src/main/resources/static/js/mystery-educator/src/pages/instruments/index.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from "react";
 import style from './style.module.scss'
-import guitar from '../../assets/acoustic-guitar.png';
-import piano from '../../assets/piano.png';
-import violin from '../../assets/violin.png';
-import drums from '../../assets/drums.png';
-import flute from '../../assets/flute.png';
+import guitar from '../../assets/guitar.gif';
+import piano from '../../assets/piano.gif';
+import violin from '../../assets/violin.gif';
+import drums from '../../assets/drums.gif';
+import flute from '../../assets/flute.gif';
 
 import Axios from 'axios';
 
@@ -35,42 +35,47 @@ const InstrumentsPage = () => {
     return (
         <div>
         <div className={style.instrument_icons}>
-        <h2>MUSIC EXPLORATION</h2>
+        <h2>MUSICAL EXPLORATION</h2>
         <div class={style.container}>
+        <h2>GUITAR</h2>
             <span><img src={guitar} class={style.image} alt="guitar"></img></span>
             <div class={style.overlay}>
-            <div class={style.projeto1}>GUITAR</div>
         </div>
         </div>
 
         <div class={style.container}>
+        <h2>PIANO</h2>
             <span><img src={piano} className={style.image} alt="piano" class="image"></img></span>
             <div class={style.overlay}>
-            <div class={style.projeto1}>PIANO</div>
         </div>
         </div>
 
         <div class={style.container}>
+        <h2>VIOLIN</h2>
             <span><img src={violin} className={style.image} alt="violin" class="image"></img></span>
             <div class={style.overlay}>
-            <div class={style.projeto1}>VIOLIN</div>
         </div>
         </div>
 
         <div class={style.container}>
+        <h2>DRUMS</h2>
             <span><img src={drums} className={style.image} alt="drums" class="image"></img></span>
             <div class={style.overlay}>
-            <div class={style.projeto1}>DRUMS</div>
         </div>
         </div>
 
         <div class={style.container}>
+        <h2>FLUTE</h2>
             <span><img src={flute} className={style.image} alt="flute" class="image"></img></span>
             <div class={style.overlay}>
-            <div class={style.projeto1}>FLUTE</div>
         </div>
         </div>
     </div>
+
+    {/* <div class={style.band}>
+            <img src={band} className={style.kid_band} alt="band" class="picture"></img>
+        </div> */}
+
             {/* {loading ? <h3>Loading...</h3> : instruments.map(instrument => (
                 <a key={instrument.id} href={`instruments/${instrument.id}`}>
                     <p>{instrument.instrumentName}</p>
