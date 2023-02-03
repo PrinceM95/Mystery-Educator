b2477cd88beabf9022bc25f0309ad20ff340c7e0
campbene <campbell20nate@gmail.com>
2023-02-02 20:49:04 -0500

    swapped contact and about, added short bio for Donna

--- src/main/resources/static/js/mystery-educator/src/components/header/index.js
+++ src/main/resources/static/js/mystery-educator/src/components/header/index.js
@@ -8,11 +8,11 @@ const Header = () => (
         <h1><center>JAC ENTERPRISES</center></h1>
         <ul className={style.navList}>
             <NavLink to='/home'>HOME</NavLink>
-            <a href='http://127.0.0.1:5500/index.html' target='_blank'>ABOUT </a>
+            <a href='http://127.0.0.1:5500/index.html' target='_blank'>CONTACT</a>
             <NavLink to='/bored'>I'M BORED, HELP!</NavLink>
             <NavLink to='/instruments'>INSTRUMENTS</NavLink>
             <NavLink to='/nasa'>NASA</NavLink>
-            <NavLink to='/contact'>CONTACT</NavLink>
+            <NavLink to='/contact'>ABOUT</NavLink>
         </ul>
     </div>
 );
